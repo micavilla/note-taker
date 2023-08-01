01c9f53934b895b62cbc19ae0aae8392fb7a8c1e
micavilla <micaella.villa1@gmail.com>
2023-07-31 22:18:54 -0500

    added api get method

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(notes.slice());
+})
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
